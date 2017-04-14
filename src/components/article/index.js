dbeccc35f8cbd766cedb16beb9a6634168244a6a
lidianbin <lidianbin@baidu.com>
2017-04-14 20:38:12 +0800

    public example

--- src/components/article/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file 
- * @author: dabeen(lidianbin@baidu.com)
- * @date:   2017-04-14 14:51:33
- */
-
-import Article from './Article';
-
-export default Article;
--- src/components/icon/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file 
- * @author: dabeen(lidianbin@baidu.com)
- * @date:   2017-04-14 15:44:30
- */
-
-import Icon from './Icon';
-
-export default Icon;
