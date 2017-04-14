e2483ce6a4064c995b449ef5b4c246e2f263c3b9
lidianbin <lidianbin@baidu.com>
2017-04-14 20:33:27 +0800

    icon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,6 +11,7 @@ var env = process.env.NODE_ENV === 'testing'
   : config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
+  context: path.join(__dirname,  '../example'),
   module: {
     loaders: utils.styleLoaders({ sourceMap: config.build.productionSourceMap, extract: true })
   },
